d9628113bd42bdaf922e6c16715eb1f6791ab855
margarcuae <margar.tcuae@gmail.com>
2017-09-26 12:27:31 -0500

    fonts + caja muy bien

--- movimientos.js
+++ movimientos.js
@@ -1,146 +1,50 @@
  var layer = new collie.Layer({
-        width : 1500,
-        height : 500
+        width : 1300,
+        height : 200
     });
      
-
+//CAJA LISTO
     var listo = new collie.DisplayObject({
-        radius : 180,
         
-        fillColor : 'pink',
-        x : 30,
-        y:10
-
-
+        width: 360,
+        height: 150,
+        backgroundColor: '#8DFF33',
+        x:30,
+        y:20
     }).addTo(layer);
-
-    var ejecutando = new collie.Circle({
-        radius : 180,
-        fillColor : 'pink',
+//CAJA EJECUTANDO
+    var ejecutando = new collie.DisplayObject({
+        
+        width:360,
+        height:150,
+        backgroundColor:'pink',
         x : 500,
-        y:10
-
-
+        y:20
     }).addTo(layer);
-
-     var ejecutando = new collie.Circle({
-        radius : 180,
-        fillColor : 'pink',
+//CAJA FINALIZADO
+    var finalizado = new collie.DisplayObject({
+        width:360,
+        height:150,
+        backgroundColor:'#33FFC4',
         x : 950,
-        y:10
-
-
+        y:20
     }).addTo(layer);
 
-/*
-    var listo = new collie.DisplayObject({
-        width : 300,
-        height : 300,
-        //x : 10,
-        //y : 10,
-        backgroundColor : 'green'
-    }).addTo(layer);
-*/
 
     var arreglo = new Array(10);
-    arreglo[0] = new collie.DisplayObject({
-        width : 30,
-        height : 15,
-        y: 150,
-        velocityX : 100,
-        backgroundColor : 'black',
-        rangeX : [0,280]
-    });
 
-     var evento3 = new collie.DisplayObject({
-        width : 30,
-        height : 15,
-        y: 170,
+    arreglo[0] = new collie.Text({
+        width : 35,
+        height : 35,
+        y: 95,
         velocityX : 100,
-        backgroundColor : 'white',
-        rangeX : [0,50]
-    }).addTo(layer);
-
-     var evento4 = new collie.DisplayObject({
-        width : 30,
-        height : 15,
-        y: 170,
-        velocityX : 100,
-        backgroundColor : 'white',
-        rangeX : [0,90]
-    }).addTo(layer);
-
-          var evento4 = new collie.DisplayObject({
-        width : 30,
-        height : 15,
-        y: 170,
-        velocityX : 100,
-        backgroundColor : 'white',
-        rangeX : [0,130]
-    }).addTo(layer);
-         var evento4 = new collie.DisplayObject({
-        width : 30,
-        height : 15,
-        y: 170,
-        velocityX : 100,
-        backgroundColor : 'white',
-        rangeX : [0,170]
-    }).addTo(layer);
-              var evento4 = new collie.DisplayObject({
-        width : 30,
-        height : 15,
-        y: 170,
-        velocityX : 100,
-        backgroundColor : 'white',
-        rangeX : [0,210]
-    }).addTo(layer);
-                   var evento4 = new collie.DisplayObject({
-        width : 30,
-        height : 15,
-        y: 170,
-        velocityX : 100,
-        backgroundColor : 'white',
-        rangeX : [0,250]
-    }).addTo(layer);
-                        var evento4 = new collie.DisplayObject({
-        width : 30,
-        height : 15,
-        y: 170,
-        velocityX : 100,
-        backgroundColor : 'white',
-        rangeX : [0,290]
-    }).addTo(layer);
-                             var evento4 = new collie.DisplayObject({
-        width : 30,
-        height : 15,
-        y: 170,
-        velocityX : 100,
-        backgroundColor : 'white',
-        rangeX : [0,330]
-    }).addTo(layer);
-
-var oText = new collie.Text({
-    width : 40, // 너비와 높이를 반드시 지정해야 합니다.
-    height : 40,
-    //x : 330,
-    y : 170,
-    backgroundColor:'red',
-    velocityX: 100,
-    fontSize: 10,
-    rangeX : [0,330],
-    fontColor : "#000000"
-}).text("  Hola").addTo(layer);
-
-
-
-
+        backgroundColor : '#6B33FF',
+        fontSize:30,
+        rangeX:[0,330],
+        fontColor: "black",
+    }).text(" 1").addTo(layer);
 
     collie.Timer.repeat(function (e) {
-        /*item.set({
-            x : item.get("x") + 10,
-            y : item.get("y") + 10
-        });
-        */
         console.log(e.count);
         if(e.count == 5){
             //collie.Renderer.stop();
@@ -152,23 +56,11 @@ var oText = new collie.Text({
             arreglo[0].set("rangeX", [280,500]);
         }
 
-
-        if (e.count ==3) {
-         var evento = new collie.DisplayObject({
-        width : 30,
-        height : 15,
-        y: 150,
-        velocityX : 100,
-        backgroundColor : 'white',
-        rangeX : [0,240]
-    }).addTo(layer);   
-        }
-        console.log("Hola");
 //        collie.Renderer.start();
     }, 1000,{loop:0});
      
     
     collie.Renderer.addLayer(layer);
-    collie.Renderer.load(document.getElementById("container"));
+    collie.Renderer.load(document.getElementById("contenedor"));
     
     collie.Renderer.start();
